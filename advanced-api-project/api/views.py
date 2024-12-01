f94a4adb43241b394ea9b38dd48dc54cc419cace
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2024-12-01 18:08:17 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from django_filters.rest_framework import DjangoFilterBackend  # Corrected impor
 from rest_framework.filters import SearchFilter, OrderingFilter  # Importing filters for search and ordering
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 # Book List View with filtering, searching, and ordering capabilities
 class BookListView(generics.ListAPIView):
