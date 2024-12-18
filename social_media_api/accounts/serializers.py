5da1192ad35aa9169a50407a4c6b06c42ec27d58
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2024-12-18 05:26:43 +0200

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,30 +1,20 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
-
-from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    token = serializers.CharField(read_only=True)
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password', 'token']
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)  # Retrieve password from validated data
-        user = User.objects.create_user(**validated_data)  # Create user with remaining fields
-        if password:
-            user.set_password(password)  # Set the password
-            user.save()  # Save the user with the new password
-        token, _ = Token.objects.get_or_create(user=user)  # Create or retrieve token for user
-        validated_data['token'] = token.key  # Add token to validated data
-        return validated_data
-
-
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+        )
+        return user
 
