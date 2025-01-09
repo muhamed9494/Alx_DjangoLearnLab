5f773e4677d4415e1675f3aee1acae4cccd3e85a
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2025-01-09 02:21:24 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 
 
