a2eb8e8b77d91f5e16058e81c0869f690510f63e
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2025-01-11 04:00:43 +0200

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.decorators import login_required, user_passes_test
-
+from django.contrib.auth.decorators import user_passes_test
 
 
 
@@ -27,7 +26,26 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')  # Redirect to login after successful registration
     template_name = 'registration/register.html'
 
+# Helper functions to check roles
+def is_admin(user):
+    return user.is_authenticated and user.profile.role == 'Admin'
 
+def is_librarian(user):
+    return user.is_authenticated and user.profile.role == 'Librarian'
 
+def is_member(user):
+    return user.is_authenticated and user.profile.role == 'Member'
+    
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html', {'message': 'Welcome, Admin!'})
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html', {'message': 'Welcome, Librarian!'})
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html', {'message': 'Welcome, Member!'})
 
 
