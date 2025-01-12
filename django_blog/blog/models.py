0183608395b6501077e551cdab477abb7b83ee85
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2025-01-12 19:26:46 +0200

    Add files via upload

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)!66ka(=-$(0)u4o3o+77ntdcg$&kya(ygkz1!=k%z9*v_0jxa'
+SECRET_KEY = 'django-insecure-$em=nj#7bgms$+nlklyngmfc47ybfw1h2$)+wgy0puc0tn9)=$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'blog/templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,11 +78,11 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'try_zema',
+        'NAME': 'app',
         'USER': 'root',
         'PASSWORD': '159_753-Mo@fZ',
-        'HOST': 'localhost',
-        'PORT': 3306,
+        'HOST': 'localhost',  
+        'PORT': 3306,  # 
     }
 }
 
@@ -120,13 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "blog/static",  
-]
