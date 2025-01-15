e515b8a74bc16746d494bf5411b0b9c7f1672d7c
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2025-01-16 00:38:49 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -45,7 +45,7 @@ def member_view(request):
 
 
 
-@permission_required('relationship_app.can_add_custom_book', raise_exception=True)
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -57,7 +57,7 @@ def add_book(request):
 
     return render(request, 'relationship_app/add_book.html')
 
-@permission_required('relationship_app.can_change_custom_book', raise_exception=True)
+@permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     
@@ -71,7 +71,7 @@ def edit_book(request, pk):
 
     return render(request, 'relationship_app/edit_book.html', {'book': book})
 
-@permission_required('relationship_app.can_delete_custom_book', raise_exception=True)
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     
