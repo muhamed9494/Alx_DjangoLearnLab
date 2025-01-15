95800a117389b5a227660d10ef4fca75771582b6
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2025-01-15 06:07:08 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -28,27 +28,33 @@ class SignUpView(CreateView):
     success_url = reverse_lazy('login')  # Redirect to login after successful registration
     template_name = 'registration/register.html'
 
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
+
 
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin_view.html')
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
-def is_member(user):
-    return user.userprofile.role == 'Member'
+
+
+
 
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'member_view.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
 
