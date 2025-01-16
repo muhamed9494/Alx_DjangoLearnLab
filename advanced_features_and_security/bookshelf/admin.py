9dfe25dc7a3df2c237afb4ca254289326ab367f4
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2025-01-16 04:00:05 +0200

    Delete advanced_features_and_security directory

--- advanced_features_and_security/bookshelf/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns in the admin list view
-    search_fields = ('title', 'author')  # Enable search functionality
-    list_filter = ('publication_year',)  # Filter options for publication year
