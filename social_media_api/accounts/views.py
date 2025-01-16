dede48d0bb2a59d5cc9b1e0764f81e99555d17e4
muhamed9494 <129572431+muhamed9494@users.noreply.github.com>
2025-01-16 06:35:07 +0200

    Add files via upload

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,10 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
 from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
@@ -28,3 +32,19 @@ class LoginView(APIView):
             token, _ = Token.objects.get_or_create(user=user)
             return Response({'token': token.key, 'user_id': user.id})
         return Response({'error': 'Invalid credentials'}, status=400)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.add(user_to_follow)
+    return Response({"message": "Followed successfully!"}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({"message": "Unfollowed successfully!"}, status=status.HTTP_200_OK)
+
+
